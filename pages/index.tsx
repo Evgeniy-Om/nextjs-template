057a0eefcd747f1b5e8be8bb4fe8bdbdcbb8d54d
Evgeniy-Om <evgenicuss@gmail.com>
2022-09-18 22:43:00 +0300

    component blur image is ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,91 +1,123 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next"
 import { useEffect, useState } from "react"
-import Link from 'next/link'
+import Link from "next/link"
 import { Blurhash } from "react-blurhash"
 import clsx from "clsx"
+import Image from "../src/components/Image"
+import { log } from "util"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
 const Thing: React.FC<{ msg: string }> = (props) => {
-    return <div>{props.msg}</div>
+  return <div>{props.msg}</div>
 }
 
 const small =
     "https://addons-media.operacdn.com/media/CACHE/images/extensions/65/117365/1.0.0.1-rev1/images/ccece86b-701f-4bdc-aff2-0a3cd27d5e47/329c447a7462471b4511fc650b94e98d.jpg"
 const medium = "https://static.base64.guru/img/logo.png"
 const large = "https://i.stack.imgur.com/N4TSy.jpg"
-const extraLarge = "http://www.wincore.ru/uploads/posts/2018-06/1527839499_wlppr4k-6.jpg"
+const extraLarge =
+    "http://www.wincore.ru/uploads/posts/2018-06/1527839499_wlppr4k-6.jpg"
+
+const images = [
+  "https://wallpaperaccess.com/full/327006.jpg",
+  "https://wallpaperaccess.com/full/360436.jpg",
+  "https://wallpaperaccess.com/full/1356237.jpg",
+  "https://wallpaperaccess.com/full/1356282.jpg",
+  "https://wallpaperaccess.com/full/1356284.jpg",
+  "https://wallpaperaccess.com/full/1356018.jpg",
+  "https://www.teahub.io/photos/full/99-995758_nature-images-hd-large-size.jpg",
+    extraLarge
+]
 
 const Home: NextPage = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [isBlurLoaded, setIsBlurLoaded] = useState(false)
-    const [url, setUrl] = useState<string>(small)
+  return (
+    <div>
+      Hello World. <Thing msg="hello" />
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <br />
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
+      {images.map((item, index) => (
+        <Image url={item} height={450} width={700} key={item} />
+      ))}
+        </div>
+      <br />
+
+    {/*    <LazyLoadImage*/}
+    {/*        src={images[0]}*/}
+    {/*        width="700px"*/}
+    {/*        height="500px"*/}
+    {/*        loading='lazy'*/}
+    {/*        onLoad={()=>console.log(33333)}*/}
+    {/*    />*/}
+    {/*    <br />*/}
+    {/*    <LazyLoadImage*/}
+    {/*        src={images[1]}*/}
+    {/*        width="700px"*/}
+    {/*        height="500px"*/}
+    {/*        onLoad={()=>console.log(444)}*/}
+    {/*    />*/}
+    {/*    <br />*/}
+    {/*    <LazyLoadImage*/}
+    {/*        src={images[2]}*/}
+    {/*        width="700px"*/}
+    {/*        height="500px"*/}
+    {/*        onLoad={()=>console.log(555)}*/}
+    {/*    />*/}
+    {/*    <br /><LazyLoadImage*/}
+    {/*    src={images[3]}*/}
+    {/*    width="700px"*/}
+    {/*    height="500px"*/}
+    {/*    onLoad={()=>console.log(666)}*/}
+    {/*/>*/}
+    {/*    <br /><LazyLoadImage*/}
+    {/*    src={images[4]}*/}
+    {/*    width="700px"*/}
+    {/*    height="500px"*/}
+    {/*    onLoad={()=>console.log(7777)}*/}
+    {/*/><br />*/}
+    {/*    <LazyLoadImage*/}
+    {/*        src={images[5]}*/}
+    {/*        width="700px"*/}
+    {/*        height="500px"*/}
+    {/*        onLoad={()=>console.log(888)}*/}
+    {/*    /><br /><LazyLoadImage*/}
+    {/*    src={images[6]}*/}
+    {/*    width="700px"*/}
+    {/*    height="500px"*/}
+    {/*    onLoad={()=>console.log(999)}*/}
+    {/*/><br />*/}
 
 
-    useEffect(()=> {
 
-        const width = window.screen.width
-        const urlStr = () => {
-            if (width > 800) return extraLarge
-            if (width > 600 ) return large
-            if (width > 400) return medium
-            return small
-        }
 
-        setUrl(urlStr)
-
-        // setTimeout(()=>setIsBlurLoaded(true),2000)
-        setIsBlurLoaded(true)
-    },[])
-
-    return (
-        <div>
-            Hello World. <Thing msg="hello" />
-            <Link href="/about">
-                <a>About</a>
-            </Link>
-            <br />
-
-            <div style={{width: '700px', height: "300px", overflow: 'hidden'}}>
-                {isLoading && <Blurhash
-                    hash={'U79~:9I:1Ha0~CJ756R5jE-;D%Rj}[$*D*NF'}
-                    width="100%"
-                    height='100%'
-                    resolutionX={32}
-                    resolutionY={32}
-                    punch={1}
-                />}
-                {isBlurLoaded && <img className={clsx({ "visually-hidden": isLoading })} src={url} onLoad={()=>setIsLoading(false)} width='100%' height='100%'/>}
-            </div>
-            <br />
-
-            {/*<LazyLoadImage*/}
-            {/*    // className={clsx({ "visually-hidden": isLoading })}*/}
-            {/*    key={'sdsdf'}*/}
-            {/*    src={url}*/}
-            {/*    width="100%"*/}
-            {/*    height='100%'*/}
-            {/*/>*/}
-
-            {/*<picture*/}
-            {/*    className={clsx({ "visually-hidden": isLoading })}*/}
-            {/*    onLoad={() => {*/}
-            {/*        console.log(2222)*/}
-            {/*        setIsLoading(false)*/}
-            {/*    }}*/}
-
-            {/*>*/}
-            {/*    <source srcSet={extraLarge} media="(min-width: 800px)" />*/}
-            {/*    <source srcSet={large} media="(min-width: 600px)" />*/}
-            {/*    <source srcSet={medium} media="(min-width: 400px)" />*/}
-            {/*    <source srcSet={small} media="(min-width: 300px)" />*/}
-
-            {/*    <img src="mdn-logo-narrow.png" alt="MDN" />*/}
-            {/*</picture>*/}
-            {/* <img
+      {/*<LazyLoadImage*/}
+      {/*    // className={clsx({ "visually-hidden": isLoading })}*/}
+      {/*    key={'sdsdf'}*/}
+      {/*    src={url}*/}
+      {/*    width="100%"*/}
+      {/*    height='100%'*/}
+      {/*/>*/}
+      {/*<picture*/}
+      {/*    className={clsx({ "visually-hidden": isLoading })}*/}
+      {/*    onLoad={() => {*/}
+      {/*        console.log(2222)*/}
+      {/*        setIsLoading(false)*/}
+      {/*    }}*/}
+      {/*>*/}
+      {/*    <source srcSet={extraLarge} media="(min-width: 800px)" />*/}
+      {/*    <source srcSet={large} media="(min-width: 600px)" />*/}
+      {/*    <source srcSet={medium} media="(min-width: 400px)" />*/}
+      {/*    <source srcSet={small} media="(min-width: 300px)" />*/}
+      {/*    <img src="mdn-logo-narrow.png" alt="MDN" />*/}
+      {/*</picture>*/}
+      {/* <img
         src={small}
         srcSet={`${large} 600w, ${medium} 400w, ${small} 300w`}
       />*/}
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Home
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,32 +1,40 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { Blurhash } from "react-blurhash"
+import clsx from "clsx"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
-const small =
-    "https://addons-media.operacdn.com/media/CACHE/images/extensions/65/117365/1.0.0.1-rev1/images/ccece86b-701f-4bdc-aff2-0a3cd27d5e47/329c447a7462471b4511fc650b94e98d.jpg"
-const medium = "https://static.base64.guru/img/logo.png"
-const large = "https://i.stack.imgur.com/N4TSy.jpg"
-const extraLarge = "http://www.wincore.ru/uploads/posts/2018-06/1527839499_wlppr4k-6.jpg"
+interface Props {
+  url: string;
+  width: number;
+  height: number;
+}
 
-const Image = () => {
-    // const [isLoading, setIsLoading] = useState(true)
-    const [url, setUrl] = useState<string>()
+const Image = (props: Props) => {
+    const {url, width, height} = props
+    const [isLoading, setIsLoading] = useState(true)
 
-
-    useEffect(()=> {
-        // const img = new Image()
-
-        const width = window.screen.width
-        if (width > 800) setUrl(extraLarge)
-        if (width > 600 && width <= 800) setUrl(large)
-        if (width > 400 && width <= 600) setUrl(medium)
-        if (width <= 400) setUrl(small)
-
-        // setUrl(img.src)
-        // img.onload = () => {
-        //     setIsLoading(false)
-        // }
-    },[])
     return (
-        <img src={url} width='100%' height='100%'/>
+      <div>
+        {isLoading && (
+          <Blurhash
+            hash={"U79~:9I:1Ha0~CJ756R5jE-;D%Rj}[$*D*NF"}
+            width={width}
+            height={height}
+            resolutionX={32}
+            resolutionY={32}
+            punch={1}
+          />
+        )}
+        <LazyLoadImage
+          className={clsx({ "visually-hidden": isLoading })}
+          src={url}
+          width={width}
+          height={height}
+          afterLoad={() => {
+            setIsLoading(false)
+          }}
+        />
+      </div>
     )
 }
 
